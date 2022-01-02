b505d4c22b7bb63ebd8127b13bd43a1e617f5ae9
Gabriel Costa <gabrielsxp@hotmail.com>
2022-01-02 17:34:41 -0300

    feat: add find tag by slug on tag controller

--- backend/src/api/tag/controllers/tag.js
+++ backend/src/api/tag/controllers/tag.js
@@ -1,9 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  *  tag controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::tag.tag');
+module.exports = createCoreController("api::tag.tag", ({ strapi }) => ({
+  async findBySlug(ctx) {
+    const { slug } = ctx.params;
+    if (!slug) {
+      ctx.status = 404;
+    } else {
+      try {
+        const data = await strapi.service("api::tag.tag").findBySlug(slug);
+        ctx.body = data;
+      } catch (error) {
+        console.error("Error getting tags by slug: ", error);
+      }
+    }
+  },
+}));
--- backend/src/api/tag/services/tag.js
+++ backend/src/api/tag/services/tag.js
@@ -1,9 +1,46 @@
-'use strict';
+"use strict";
 
 /**
  * tag service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::tag.tag');
+module.exports = createCoreService("api::tag.tag", ({ strapi }) => ({
+  async findBySlug(slug) {
+    try {
+      const templateData = await strapi
+        .service("api::global.global")
+        .getTemplateData();
+      const jobs = await strapi.db.query("api::post.post").findMany({
+        where: {
+          active: true,
+          tags: {
+            slug,
+          },
+        },
+        populate: [
+          "categories",
+          "company.profile_picture",
+          "company.posts",
+          "post_settings",
+          "tags",
+        ],
+      });
+
+      const tag = await strapi.db.query("api::tag.tag").findOne({
+        where: {
+          slug,
+        },
+      });
+
+      return {
+        tag,
+        templateData,
+        jobs: strapi.service("api::post.post").attachPostLength(jobs),
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+}));
