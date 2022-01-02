f483043472a9ba1040b0905cd2e3db58b5b03b4b
Gabriel Costa <gabrielsxp@hotmail.com>
2022-01-02 13:13:29 -0300

    fix: incongruent category job listing on same route

--- frontend/src/components/JobListSection/index.tsx
+++ frontend/src/components/JobListSection/index.tsx
@@ -1,5 +1,13 @@
 import { useState, useEffect, useMemo } from "react";
-import { Center, Flex, Heading, Select, Stack, Text } from "@chakra-ui/react";
+import {
+  Center,
+  Flex,
+  Heading,
+  Select,
+  Skeleton,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { JobCardProps } from "types";
 import JobCard from "../JobCard";
 import Section from "../Section";
@@ -9,16 +17,22 @@ type JobListSectionProps = {
   heading: String;
   description?: String;
   jobList: JobCardProps[];
+  isLoading?: Boolean;
 };
 
 const JobListSection = ({
   heading,
   description,
   jobList,
+  isLoading,
 }: JobListSectionProps) => {
   const isMobile = useIsTouchDevice();
   const [displayedJobs, setDisplayedJobs] = useState(jobList);
   const [category, setCategory] = useState("");
+
+  useEffect(() => {
+    setDisplayedJobs(jobList);
+  }, [jobList]);
 
   const allCategories = useMemo(() => {
     const jobsCategories = jobList?.reduce((acc: String[], job): String[] => {
@@ -45,18 +59,28 @@ const JobListSection = ({
   }, [category]);
 
   const getCards = () => {
+    if (isLoading) {
+      return (
+        <Stack w="100%">
+          {Array.from(new Array(10)).map((_, index) => {
+            return <Skeleton h="125px" w="100%" key={index} />;
+          })}
+        </Stack>
+      );
+    }
     if (displayedJobs?.length) {
       return displayedJobs?.map((jobCard: JobCardProps, index) => {
         return <JobCard data={jobCard} key={index} />;
       });
     }
-    return <div />;
   };
+
   return (
     <Section minW="auto">
       <Center>
         <Stack w="100%">
           <Flex
+            w="100%"
             align="center"
             justify="space-between"
             direction={isMobile ? "column" : "row"}
--- frontend/src/pages/categories/[slug].tsx
+++ frontend/src/pages/categories/[slug].tsx
@@ -1,6 +1,8 @@
-import GenericPageHero from "@/components/GenericPageHero";
+import { useMemo } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import JobListSection from "@/components/JobListSection";
-import { Box, Center, Heading, useColorMode } from "@chakra-ui/react";
+import { Box, useColorMode } from "@chakra-ui/react";
 import Template from "templates";
 import { CategoryProps, JobCardProps, TemplateDataProps } from "types";
 import {
@@ -10,7 +12,7 @@ import {
   bgColor,
   _formatCategories,
 } from "../../helpers";
-import fetch from "../../services/api";
+import fetch, { fetcher } from "../../services/api";
 
 type CategoryPageProps = {
   data: {
@@ -20,17 +22,43 @@ type CategoryPageProps = {
   };
 };
 
+const useCategoryFetch = (slug) => {
+  const { data: categoryData, error } = useSWR(`/category/${slug}`, fetcher);
+
+  return {
+    categoryData,
+    isLoading: !error && !categoryData,
+    error,
+  };
+};
+
 const CategoryPage = ({ data }: CategoryPageProps) => {
   const { colorMode } = useColorMode();
+
+  const { categoryData, isLoading } = useCategoryFetch(data?.category?.slug);
+
+  const jobs = useMemo(() => {
+    return categoryData?.jobs
+      ? applyPostsSorting(
+          categoryData?.jobs?.map((job) => _formatCardPost(job))
+        )
+      : data?.jobs;
+  }, [categoryData?.jobs]);
+
   return (
     <Template data={data?.templateData}>
-      <Box pt="120px" w="100%" bg={bgColor[colorMode]}>
+      <Box minH="50vh" pt="120px" w="100%" bg={bgColor[colorMode]}>
         <JobListSection
           heading={`Latest ${data?.category?.title} jobs`}
+          isLoading={isLoading}
           description={
-            !data?.jobs?.length && "No recent jobs found for this category!"
+            !data?.jobs?.length
+              ? "No recent jobs found for this category!"
+              : `${data.jobs?.length} ${
+                  data.jobs?.length === 1 ? "job" : "jobs"
+                } found`
           }
-          jobList={data?.jobs}
+          jobList={jobs}
         />
       </Box>
     </Template>
@@ -72,6 +100,7 @@ export async function getStaticProps({ params: { slug } }) {
     props: {
       data,
     },
+    notFound,
   };
 }
 
