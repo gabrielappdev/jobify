c41605542f2358e609146b691331414e56456bbd
Gabriel Costa <gabrielsxp@hotmail.com>
2022-01-02 12:26:45 -0300

    feat: add get category by slug on category controller

--- backend/src/api/category/controllers/category.js
+++ backend/src/api/category/controllers/category.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  *  category controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController(
+  "api::category.category",
+  ({ strapi }) => ({
+    async findBySlug(ctx) {
+      const { slug } = ctx.params;
+      if (!slug) {
+        ctx.status = 404;
+      } else {
+        try {
+          const data = await strapi
+            .service("api::category.category")
+            .findBySlug(slug);
+          ctx.body = data;
+        } catch (error) {
+          console.error("Error getting categories by slug: ", error);
+        }
+      }
+    },
+  })
+);
--- backend/src/api/category/services/category.js
+++ backend/src/api/category/services/category.js
@@ -1,9 +1,45 @@
-'use strict';
+"use strict";
 
 /**
  * category service.
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::category.category');
+module.exports = createCoreService("api::category.category", ({ strapi }) => ({
+  async findBySlug(slug) {
+    try {
+      const templateData = await strapi
+        .service("api::global.global")
+        .getTemplateData();
+      const jobs = await strapi.db.query("api::post.post").findMany({
+        where: {
+          active: true,
+          categories: {
+            slug,
+          },
+        },
+        populate: [
+          "categories",
+          "company.profile_picture",
+          "company.posts",
+          "post_settings",
+        ],
+      });
+
+      const category = await strapi.db.query("api::category.category").findOne({
+        where: {
+          slug,
+        },
+      });
+
+      return {
+        category,
+        templateData,
+        jobs: strapi.service("api::post.post").attachPostLength(jobs),
+      };
+    } catch (error) {
+      throw new Error(error);
+    }
+  },
+}));
